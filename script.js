63cebcb6008050a1e4b2b751b52b6f90fbf2b392
Maite Quiles Cerdà <maite@MacBook-MacBook-Pro-de-Maite.local>
2020-03-17 16:44:58 +0100

    added modal materials

--- script.js
+++ script.js
@@ -1,35 +1,29 @@
 "use strict";
 window.addEventListener("DOMContentLoaded", init);
 
-const userInput = document.querySelectorAll("input[type=search]");
 const apiBase = "https://frontend.natmus.dk/api/";
 const HTML = {};
-// the prototypes for all items, measurements and materials
-const Item = {
-    id: null,
-    collection: null,
-    identification: null,
-    title: null,
-    descriptions: null,
-    images: [],
-    materials: [],
-    measurements: []
-};
-const Measurement = {
-    type: null,
-    value: null,
-    unit: null
-};
-const Material = {
-    type: null,
-    color: null,
-    processing: null
-};
 
 function init(){
-    userInput.forEach(input => {
-        input.addEventListener("keyup", search);
+    HTML.inputBoxes = document.querySelectorAll("input[type=search]");
+    HTML.inputBoxes.forEach(inputBox => {
+        inputBox.addEventListener("keyup", search);
     })
+    // item list
+    HTML.itemTemplate = document.querySelector(".templates");
+    HTML.dataList = document.querySelector("#dataList");
+    // modal
+    HTML.modal = document.querySelector("#infoModal");
+    HTML.title = HTML.modal.querySelector('.modal-title');
+    HTML.collection = HTML.modal.querySelector('.collection');
+    HTML.identification = HTML.modal.querySelector('.identification');
+    HTML.imgTemplate = document.querySelector(".imgTemplate");
+    HTML.imgContainer = HTML.modal.querySelector(".carousel-inner");
+    HTML.listItemTemplate = document.querySelector(".listItemTemplate");
+    HTML.descriptionContainer = HTML.modal.querySelector("div.modal-body > div:nth-child(2)");
+    HTML.descriptionList = HTML.descriptionContainer.querySelector("ul");
+    HTML.materialContainer = HTML.modal.querySelector("div.modal-body > div:nth-child(3)");
+    HTML.materialList = HTML.materialContainer.querySelector("ul");
 }
 
 function escapeChars(word){
@@ -44,7 +38,6 @@ function escapeChars(word){
 function buildUrl(input, query){
     let requestUrl = `${apiBase}Search?query=${query}:`;
     const aWords = input.split(" ");
-    // console.log(aWords);
     aWords.forEach(word => {
         if(word === aWords[0]){
             requestUrl = requestUrl+word;
@@ -54,13 +47,14 @@ function buildUrl(input, query){
     })
     return requestUrl;
 }
+
 // To do: combine search fields
 function search(){
-    if(this.value){
-        const input = escapeChars(this.value);
-        const query = this.dataset.search;
-        const requestUrl = buildUrl(input, query)
-        // console.log(requestUrl);
+    const inputBox = this;
+    if(inputBox.value){
+        const input = escapeChars(inputBox.value);
+        const query = inputBox.dataset.search;
+        const requestUrl = buildUrl(input, query);
         getData(requestUrl);
     }
 }
@@ -68,17 +62,14 @@ function search(){
 async function getData(requestUrl){
     const response = await fetch(requestUrl);
     const jData = await response.json();
-    // console.log(jData);
     showSearch(jData);
 }
 
 function showSearch(items){
-    const itemTemplate = document.querySelector(".templates");
-    const dataList = document.querySelector("#dataList");
-    dataList.innerHTML = "";
+    HTML.dataList.innerHTML = "";
 
     items.forEach(item => {
-        const clone = itemTemplate.cloneNode(true).content;
+        const clone = HTML.itemTemplate.cloneNode(true).content;
         if(item.images && item.images.length !== 0){
             const img = clone.querySelector("img");
             img.src = `https://frontend.natmus.dk/api/Image?id=${item.images[0]}`;
@@ -87,18 +78,17 @@ function showSearch(items){
             }
         }
         clone.querySelector(".card-title").textContent = item.title;
+        clone.querySelector(".card-subtitle").textContent = item.identification;
         clone.querySelector(".collection").textContent = item.collection;
-        clone.querySelector(".identification").textContent = item.identification;
-        // console.log(item.title, item.descriptions, item.materials, item.measurements);
         if(item.descriptions && item.descriptions.length !==0 
         || item.materials && item.materials.length !== 0 
         || item.measurements && item.measurements.length !== 0){
             const btn = clone.querySelector(".btn");
             btn.dataset.id = item.id;
             btn.dataset.identification = item.identification;
-            btn.classList.add("d-block");
+            btn.classList.remove("d-none");
         }
-        dataList.appendChild(clone);
+        HTML.dataList.appendChild(clone);
     })
 }
 
@@ -109,88 +99,96 @@ $('#infoModal').on('show.bs.modal', function (event) {
     const button = $(event.relatedTarget); // Button that triggered the modal
     const itemId = button.data('id'); // Extract info from data-* attributes
     const itemId2 = escapeChars(button.data('identification'));
-    const requestData = `id:${itemId} AND identification:${itemId2}`;
     // Update the modal's content
     $.ajax({
         url: `${apiBase}Search`,
-       data: {query:requestData}
+       data: {query:`id:${itemId} AND identification:${itemId2}`}
       }).done(function(jData){
-        const modalItem = jData[0];
-        HTML.modal = document.querySelector("#infoModal");
-        HTML.modal.querySelector('.modal-title').textContent = modalItem.title;
-        HTML.modal.querySelector('.collection').textContent = modalItem.collection;
-        HTML.modal.querySelector('.identification').textContent = modalItem.identification;
-        if(modalItem.images && modalItem.images.length !== 0){
-            setModalImgs(modalItem.images, modalItem.title);
+        const item = jData[0];
+        HTML.title.textContent = item.title;
+        HTML.collection.textContent = item.collection;
+        HTML.identification.textContent = item.identification;
+        if(item.images && item.images.length !== 0){
+            setModalImgs(item.images, item.title);
         }
-        if(modalItem.descriptions && modalItem.descriptions.length !==0){
-            setModalDescriptions(modalItem.descriptions);
+        if(item.descriptions && item.descriptions.length !==0){
+            setModalList(item.descriptions, HTML.descriptionContainer, HTML.descriptionList);
+        }
+        if(item.materials && item.materials.length !==0){
+            setModalList(item.materials, HTML.materialContainer, HTML.materialList);
         }
       })
   })
 
-// Remove modal images and display of optional fields after closing
 $('#infoModal').on('hide.bs.modal', function() {
-
-    //images
+    // Remove modal images
     if(document.querySelector(".carousel-item")){
-        const imgContainer = document.querySelector(".carousel-inner");
-        while (imgContainer.hasChildNodes()) {  
-            imgContainer.removeChild(imgContainer.firstChild);
+        while (HTML.imgContainer.hasChildNodes()) {  
+            HTML.imgContainer.removeChild(HTML.imgContainer.firstChild);
         }
     }
-
-    //descriptions
-    if(HTML.descriptionContainer){
-        HTML.descriptionContainer.classList.add("d-none");
-    }
+    // Remove modal descriptions
     if(document.querySelector(".list-group-item")){
+        HTML.descriptionContainer.classList.add("d-none");
         while (HTML.descriptionList.hasChildNodes()) {  
             HTML.descriptionList.removeChild(HTML.descriptionList.firstChild);
         }
     }
 })
 
-  function setModalImgs(images, title){
-    const imgTemplate = document.querySelector(".imgTemplate");
-    const imgContainer = document.querySelector(".carousel-inner");
-    imgContainer.innerHTML = "";
+function setModalImgs(images, title){
+    HTML.imgContainer.innerHTML = "";
 
     images.forEach(img => {
-        const cloneImg = imgTemplate.cloneNode(true).content;
-        cloneImg.querySelector("img").src =`https://frontend.natmus.dk/api/Image?id=${img}`;
-        cloneImg.querySelector("img").alt = title;
+        const clone = HTML.imgTemplate.cloneNode(true).content;
+        clone.querySelector("img").src =`https://frontend.natmus.dk/api/Image?id=${img}`;
+        clone.querySelector("img").alt = title;
         if(img === images[0]){
-            //console.log(images);
-            cloneImg.querySelector(".carousel-item").classList.add("active");
+            clone.querySelector(".carousel-item").classList.add("active");
         }
-        imgContainer.appendChild(cloneImg);
+        HTML.imgContainer.appendChild(clone);
+    })
+}
+
+function buildString(material){
+    let newString = "";
+    if(material.type){
+        newString = newString + material.type;
+        if(material.color || material.processing){
+            newString = newString + ": ";
+        }
+    }
+    if(material.color){
+        newString = newString + material.color;
+        if(material.processing){
+            newString = newString + ", ";
+        }
+    }
+    if(material.processing){
+        newString = newString + material.processing;
+    }
+    return newString;
+}
+
+  function setModalList(data, displayBox, innerBox){
+    displayBox.classList.remove("d-none");
+    innerBox.innerHTML = "";
+
+    data.forEach(dataElement => {
+        const clone = HTML.listItemTemplate.cloneNode(true).content;
+        let text = dataElement;
+        if(typeof text === "object"){
+            text = buildString(dataElement);
+        }
+        clone.querySelector("li").textContent = text;
+        innerBox.appendChild(clone);
     })
   }
-
-  function setModalDescriptions(descriptions){
-      //console.log("descriptions: ", descriptions);
-      HTML.descriptionContainer = HTML.modal.querySelector("div.modal-body > div:nth-child(2)");
-      HTML.descriptionContainer.classList.remove("d-none");
-
-      HTML.descriptionTemplate = document.querySelector(".descriptionTemplate");
-      HTML.descriptionList = HTML.descriptionContainer.querySelector("ul");
-      //console.log(HTML.descriptionList);
-      HTML.descriptionList.innerHTML = "";
-
-      descriptions.forEach(description => {
-          //console.log(description);
-          const cloneDescription = HTML.descriptionTemplate.cloneNode(true).content;
-          cloneDescription.querySelector("li").textContent = description;
-          HTML.descriptionList.appendChild(cloneDescription);
-      })
-    }
 
 // ---------------------------------- Modal end ----------------------------------
 
 //to do:
-// modal materials and measurements
-// HTML object
+// modal measurements
 // Search by name, identification, collection, etc.
 // load more results button
 // document code
